9ca245657ef32ad64031569d5c8411d92579e025
Diego Borges <diegoaragao465@gmail.com>
2025-04-29 16:02:06 -0300

    Changing factories to be a singleton

--- src/main/java/me/diego/factory/ExpressOrderFactory.java
+++ src/main/java/me/diego/factory/ExpressOrderFactory.java
@@ -3,6 +3,17 @@ package me.diego.factory;
 import me.diego.domain.Order;
 
 public class ExpressOrderFactory implements OrderFactory {
+  private static ExpressOrderFactory instance;
+
+  private ExpressOrderFactory() {}
+
+  public static ExpressOrderFactory getInstance() {
+    if (instance == null) {
+      instance = new ExpressOrderFactory();
+    }
+
+    return instance;
+  }
 
   @Override
   public Order createOrder(String orderId, String restaurantName) {
--- src/main/java/me/diego/factory/StandardOrderFactory.java
+++ src/main/java/me/diego/factory/StandardOrderFactory.java
@@ -3,6 +3,18 @@ package me.diego.factory;
 import me.diego.domain.Order;
 
 public class StandardOrderFactory implements OrderFactory {
+  private static StandardOrderFactory instance;
+
+  private StandardOrderFactory() {}
+
+  public static StandardOrderFactory getInstance() {
+    if (instance == null) {
+      instance = new StandardOrderFactory();
+    }
+
+    return instance;
+  }
+
   @Override
   public Order createOrder(String orderId, String restaurantName) {
     var order = new Order(orderId, restaurantName);
